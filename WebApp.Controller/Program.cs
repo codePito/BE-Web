1441f2ed10f446b78e682f28e7b71fbb59dac32d
codePito <105501650+codePito@users.noreply.github.com>
2025-12-25 09:39:20 +0700

    Delete ConfigureHttpRequest

--- WebApp.Controller/Program.cs
+++ WebApp.Controller/Program.cs
@@ -169,11 +169,11 @@ app.UseStaticFiles();
 app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
--- WebApp.Model/Data/WebContext.cs
+++ WebApp.Model/Data/WebContext.cs
@@ -10,7 +10,6 @@ namespace WebApp.Model.Data
         public DbSet<Product>? Products { get; set; }
         public DbSet<User>? Users { get; set; }
         public DbSet<Category>? Categories { get; set; }
-        //public DbSet<ProductImage> ProductImages { get; set; }
         public DbSet<Cart> Carts { get; set; }
         public DbSet<CartItem> CartItems { get; set; }
         public DbSet<Order> Orders { get; set; }
