7ecb8627b6e38e8b419ca580954b660d0079ee32
codePito <105501650+codePito@users.noreply.github.com>
2026-01-10 16:20:42 +0700

    Fix UserResponse

--- WebApp.Model/Response/UserResponse.cs
+++ WebApp.Model/Response/UserResponse.cs
@@ -10,6 +10,7 @@ namespace WebApp.Model.Response
     {
         public int Id { get; set; }
         public string UserName { get; set; } = string.Empty;
+        public string? Email { get; set; }
         public string Address { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
